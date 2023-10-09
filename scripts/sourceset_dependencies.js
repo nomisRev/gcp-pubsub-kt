7e1d66238454e983dbbee636355206c56fdec72e
nomisRev <nomisRev@users.noreply.github.com>
2023-10-09 17:20:22 +0000

    deploy: 72f151518a0f1b1bc74bca95b254a7921596c59e

---
+++ scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":gcp-pubsub:dokkaHtmlPartial/main":[],":google-common-api:dokkaHtmlPartial/main":[],":gcp-pubsub-test:dokkaHtmlPartial/main":[],":gcp-pubsub-ktor-kotlinx-serialization-json:dokkaHtmlPartial/main":[],":gcp-pubsub-kotlinx-serialization-json:dokkaHtmlPartial/main":[],":gcp-pubsub-ktor:dokkaHtmlPartial/main":[]}'
